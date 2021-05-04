0ffc37ae94f36f84b2daf1410c25abc52a8a4f59
snootiecute <dangtiendat288@gmail.com>
2021-05-04 09:59:35 +0700

    complete FirebaseMusicSource

--- app/src/main/java/com/example/musicplayer/exoplayer/FirebaseMusicSource.kt
+++ app/src/main/java/com/example/musicplayer/exoplayer/FirebaseMusicSource.kt
@@ -1,16 +1,71 @@
 package com.example.musicplayer.exoplayer
 
-class FirebaseMusicSource {
+import android.media.MediaMetadata
+import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.MediaBrowserCompat.MediaItem.FLAG_PLAYABLE
+import android.support.v4.media.MediaDescriptionCompat
+import android.support.v4.media.MediaMetadataCompat
+import android.support.v4.media.MediaMetadataCompat.*
+import androidx.core.net.toUri
+import com.example.musicplayer.data.remote.MusicDatabase
+import com.example.musicplayer.exoplayer.State.*
+import com.google.android.exoplayer2.source.ConcatenatingMediaSource
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class FirebaseMusicSource @Inject constructor (
+    private val musicDatabase: MusicDatabase
+) {
+
+    var songs = emptyList<MediaMetadataCompat>()
+    suspend fun fetchMediaData() = withContext(Dispatchers.IO) {
+        state = STATE_INITIALIZING
+        val allSongs = musicDatabase.getAllSongs()
+        songs = allSongs.map{ song ->
+            MediaMetadataCompat.Builder()
+                .putString(METADATA_KEY_MEDIA_ID, song.mediaId)
+                .putString(METADATA_KEY_TITLE, song.title)
+                .putString(METADATA_KEY_DISPLAY_TITLE, song.title)
+                .putString(METADATA_KEY_DISPLAY_ICON_URI, song.imageUrl)
+                .putString(METADATA_KEY_ALBUM_ART_URI, song.imageUrl)
+                .putString(METADATA_KEY_MEDIA_URI, song.songUrl)
+                .build()
+        }
+        state = STATE_INITIALIZED
+    }
+
+    fun asMediaSource(dataSourceFactory: DefaultDataSourceFactory): ConcatenatingMediaSource{
+        val concatenatingMediaSource = ConcatenatingMediaSource()
+        songs.forEach{song ->
+            val mediaSource = ProgressiveMediaSource.Factory(dataSourceFactory)
+                .createMediaSource(song.getString(METADATA_KEY_MEDIA_URI).toUri())
+            concatenatingMediaSource.addMediaSource(mediaSource)
+        }
+        return concatenatingMediaSource
+    }
+
+    fun asMediaItems() = songs.map{song ->
+        val desc = MediaDescriptionCompat.Builder()
+            .setMediaUri(song.getString(METADATA_KEY_MEDIA_URI).toUri())
+            .setTitle(song.description.title)
+            .setMediaId(song.description.mediaId)
+            .setIconUri(song.description.iconUri)
+            .build()
+        MediaBrowserCompat.MediaItem(desc, FLAG_PLAYABLE)
+    }
 
     private val onReadyListeners = mutableListOf<(Boolean) -> Unit>()
 
-    private var state:State = State.STATE_CREATED
+    private var state:State = STATE_CREATED
         set(value) {
-            if(value == State.STATE_INITIALIZED || value==State.STATE_ERROR){
+            if(value == STATE_INITIALIZED || value== STATE_ERROR){
                 synchronized(onReadyListeners){
                     field = value
                     onReadyListeners.forEach{
-                        listener -> listener(state == State.STATE_INITIALIZED)
+                        listener -> listener(state == STATE_INITIALIZED)
                     }
                 }
             } else {
@@ -19,11 +74,11 @@ class FirebaseMusicSource {
         }
 
     fun whenReady(action: (Boolean)->Unit):Boolean{
-        if(state == State.STATE_CREATED || state == State.STATE_INITIALIZING){
+        if(state == STATE_CREATED || state == STATE_INITIALIZING){
             onReadyListeners += action
             return false
         } else {
-            action(state == State.STATE_INITIALIZED)
+            action(state == STATE_INITIALIZED)
             return true
         }
     }
